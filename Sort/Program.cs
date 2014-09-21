69f5187151cd0fc665f6b2181799d8d2274b4fa0
TheMoo75 <sbmoody@gmail.com>
2014-09-21 20:53:33 +0100

    Added project files

---
+++ Sort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quicksort quicksort = new Quicksort(); 
+
+            int[] unsorted = {5, 3, 1, 2, 10, 7, 6, 4, 9, 8};
+
+            Console.WriteLine("Unsorted");
+            Console.WriteLine();
+
+            for (int count = 0; count < unsorted.Count(); count++)
+            {
+                Console.WriteLine(unsorted[count]);
+            }
+
+            int[] sorted = quicksort.Sort(unsorted);
+
+            Console.WriteLine();
+            Console.WriteLine("Sorted");
+            Console.WriteLine();
+
+            for (int count = 0; count < sorted.Count(); count++)
+            {
+                Console.WriteLine(unsorted[count]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sort/quicksort.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    /// <summary>
+    /// A version of Quicksort sourced from Introduction to Algorithms (Third Edition)
+    /// chapter 7.1 Description of quicksort, page 171.
+    /// </summary>
+    class Quicksort
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+        public int[] Sort(int[] elements)
+        {
+            Quick_Sort(elements, 0, elements.Count() - 1);
+            return elements;
+        }
+
+        private void Quick_Sort(int[] elements, int left, int right)
+        {
+            int partition;
+            if (left < right)
+            {
+                partition = Partition(elements, left, right);
+                Quick_Sort(elements, left, partition - 1);
+                Quick_Sort(elements, partition + 1, right);
+            }
+        }
+
+        private int Partition(int[] elements, int left, int right)
+        {
+            int pivot = elements[right];
+            int index = left - 1;
+            // The book suggests using: for (int count = left; count < right - 1; count++)
+            // but this produces an incomplete sort.
+            for (int count = left; count < right; count++)
+            {
+                if (elements[count] < pivot)
+                {
+                    index++;
+                    Swap(elements, index, count);
+                }
+            }
+            Swap(elements, index + 1, right);
+            return index + 1;
+        }
+
+        private void Swap(int[] elements, int left, int right)
+        { 
+            int temp = elements[left];
+            elements[left] = elements[right];
+            elements[right] = temp;
+        }
+    }
+}
